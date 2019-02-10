11affe7f3b8fe3e08bcde0a32844d32e4d09d97a
wooguylee <dlwjddnbest@gmail.com>
2019-02-10 14:06:00 +0900

    first file

---
+++ testTkinter.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 02:57:50 2019
+
+@author: dlwjddnbest
+"""
+
+from tkinter import *
+
+def sel():
+    selection = "Value = " + str(var.get())
+    label.config(text=selection)
+
+def chg(value):
+    selection = "Change = " + str(value)
+    label.config(text=selection)
+    
+
+root = Tk()
+root.geometry('800x480+500+500')
+#root.attributes("-fullscreen", True)
+var = DoubleVar()
+scale = Scale(root, variable=var, command=chg, from_=6, to=-20)
+scale.place(x=100, y=100, width=100, height=300)
+#scale.pack(anchor=W)
+
+button = Button(root, text="Get Scale Value", command=sel)
+button.pack(anchor=E)
+
+label = Label(root)
+label.pack()
+
+root.mainloop()
