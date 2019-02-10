799ce9638842da4a25d4c89abb4f586840d8e884
wooguylee <dlwjddnbest@gmail.com>
2019-02-10 14:56:07 +0900

    asdf

--- testTkinter.py
+++ testTkinter.py
@@ -1,31 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 10 02:57:50 2019
-
-@author: dlwjddnbest
-"""
-
 #from tkinter import *
 from Tkinter import *
 
 def sel():
-    selection = "Value = " + str(var.get())
+    selection = "값 = " + str(var.get())
     label.config(text=selection)
 
 def chg(value):
-    selection = "Change = " + str(value)
+    selection = "변경 = " + str(value)
     label.config(text=selection)
     
 
 root = Tk()
 #root.geometry('800x480+500+500')
 root.attributes("-fullscreen", True)
+
 var = DoubleVar()
 scale = Scale(root, variable=var, command=chg, from_=6, to=-20)
 scale.place(x=100, y=100, width=100, height=300)
 #scale.pack(anchor=W)
 
-button = Button(root, text="Get Scale Value", command=sel)
+button = Button(root, text="값 가져오기", command=sel)
 button.pack(anchor=E)
 
 label = Label(root)
